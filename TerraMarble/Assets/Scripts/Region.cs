128c779e62bb4a85a1013ce3aaa744df7384d8ac
Ethan Moffatt <102685218@student.swin.edu.au>
2022-11-10 10:56:06 +1100

    Initial step towards spawning on wheel

--- TerraMarble/Assets/Scripts/Region.cs
+++ TerraMarble/Assets/Scripts/Region.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Shapes;
 using UnityEngine;
 
 public class Region : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform _anchor = null;
+    public Wheel Wheel = null;
+    public Disc Disc = null;
+
+    public Transform Anchor
     {
-        
+        get
+        {
+            if (_anchor == null)
+            {
+                _anchor = new GameObject("Anchor").transform;
+                _anchor.SetParent(transform, false);
+                // _anchor.position
+            }
+            return _anchor;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector3 EndPosition => Anchor.position;
 }
