1d3cea7421098456c489fc630594ae20f903dcea
Ethan Moffatt <102685218@student.swin.edu.au>
2022-11-09 14:42:24 +1100

    Refactored Wheel, added Wheel Generator

--- TerraMarble/Assets/Scripts/Wheel.cs
+++ TerraMarble/Assets/Scripts/Wheel.cs
@@ -1,15 +1,8 @@
-using Shapes;
 using UnityEngine;
 
-[ExecuteAlways]
+
 public class Wheel : MonoBehaviour
 {
-    [Header("Generation")]
-    public bool doCreateRegions = false;
-    public int regionCount = 36;
-    public float regionRadius = 3f;
-    public GameObject pregenRegionDefault = null;
-
     [Header("Debug")]
     public Transform regionsParent;
     public Region[] regions;
@@ -21,57 +14,6 @@ public class Wheel : MonoBehaviour
 
     private void Update()
     {
-        if (doCreateRegions)
-        {
-            doCreateRegions = false;
-            ClearRegions();
-            CreateRegions();
-        }
 
-    }
-
-    public void ClearRegions()
-    {
-        Region[] childRegions = regionsParent.GetComponentsInChildren<Region>();
-        foreach (Region childRegion in childRegions)
-        {
-            if (Application.isEditor && !Application.isPlaying)
-                DestroyImmediate(childRegion.gameObject);
-            else Destroy(childRegion.gameObject);
-        }
-        regions = null;
-    }
-
-    public void CreateRegions()
-    {
-        if (pregenRegionDefault == null)
-        {
-            Debug.LogWarning("Regions could not generate. Missing prefab.");
-            return;
-        }
-
-        Region[] newRegions = new Region[regionCount];
-
-        float radianSize = Mathf.PI * 2f / regionCount;
-        float previousRadians = 0f;
-        for (int i = 0; i < regionCount; i++)
-        {
-            GameObject newGO = GameObject.Instantiate(pregenRegionDefault, Vector3.zero, Quaternion.identity, regionsParent);
-
-            Region newRegion = newGO.GetComponent<Region>();
-            Disc newDisc = newGO.GetComponentInChildren<Disc>();
-            // Set array
-            newRegions[i] = newRegion;
-            // Config
-            newGO.name = "Region " + i.ToString("D2");
-            newDisc.Radius = regionRadius;
-            newDisc.AngRadiansStart = previousRadians;
-            float newRadians = previousRadians + radianSize;
-            newDisc.AngRadiansEnd = newRadians;
-            // End
-            previousRadians = newRadians;
-        }
-
-        regions = newRegions;
     }
 }
---
+++ TerraMarble/Assets/Scripts/WheelGenerator.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Shapes;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WheelGenerator : MonoBehaviour
+{
+    [Header("Config")]
+    public Wheel wheel;
+    public bool doCreateRegions = false;
+    public int regionCount = 36;
+    public float regionRadius = 3f;
+    public GameObject pregenRegionDefault = null;
+
+    void Awake()
+    {
+        wheel ??= GetComponent<Wheel>();
+    }
+    
+    void Update()
+    {
+        if (doCreateRegions)
+        {
+            doCreateRegions = false;
+            ClearRegions();
+            CreateRegions();
+        }
+    }
+    public void ClearRegions()
+    {
+        wheel ??= GetComponent<Wheel>();
+
+        Region[] childRegions = wheel.regionsParent.GetComponentsInChildren<Region>();
+        foreach (Region childRegion in childRegions)
+        {
+            if (Application.isEditor && !Application.isPlaying)
+                DestroyImmediate(childRegion.gameObject);
+            else Destroy(childRegion.gameObject);
+        }
+        wheel.regions = null;
+    }
+
+    public void CreateRegions()
+    {
+        if (pregenRegionDefault == null)
+        {
+            Debug.LogWarning("Regions could not generate. Missing prefab.");
+            return;
+        }
+        wheel ??= GetComponent<Wheel>();
+
+        Region[] newRegions = new Region[regionCount];
+
+        float radianSize = Mathf.PI * 2f / regionCount;
+        float previousRadians = 0f;
+        for (int i = 0; i < regionCount; i++)
+        {
+            GameObject newGO = GameObject.Instantiate(pregenRegionDefault, Vector3.zero, Quaternion.identity, wheel.regionsParent);
+
+            Region newRegion = newGO.GetComponent<Region>();
+            Disc newDisc = newGO.GetComponentInChildren<Disc>();
+            // Set array
+            newRegions[i] = newRegion;
+            // Config
+            newGO.name = "Region " + i.ToString("D2");
+            newDisc.Radius = regionRadius;
+            newDisc.AngRadiansStart = previousRadians;
+            float newRadians = previousRadians + radianSize;
+            newDisc.AngRadiansEnd = newRadians;
+            // End
+            previousRadians = newRadians;
+        }
+
+        wheel.regions = newRegions;
+    }
+}
