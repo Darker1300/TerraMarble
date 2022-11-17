4f3f0c0b1cf63eb03f1bb37960556e90ad3daacc
Ethan Moffatt <102685218@student.swin.edu.au>
2022-11-17 18:15:53 +1100

    Added Walkers to scene Ethan 4.

---
+++ TerraMarble/Assets/Scripts/Walker.cs
@@ -0,0 +1,56 @@
+using MathUtility;
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    private Wheel wheel = null;
+    private Region regionLeader = null;
+
+    [Header("Config")]
+    public float speed = 3f;
+
+    public Vector2 targetWheelPos = new Vector2(0f, 1f);
+    [Header("Debug")]
+    public Vector2 currentWheel = new Vector2(0f, 1f);
+    public Vector2 currentGoalWheel = new Vector2(0f, 1f);
+
+
+    void Awake()
+    {
+        wheel ??= GameObject.FindGameObjectWithTag("Wheel").GetComponent<Wheel>();
+        regionLeader ??= wheel.regions[0];
+
+        currentWheel = regionLeader.WorldToRegion(transform.position);
+        targetWheelPos = currentWheel;
+        currentGoalWheel = targetWheelPos;
+    }
+
+    void Update()
+    {
+        currentWheel = regionLeader.WorldToRegion(transform.position);
+
+        float currentSpeed = speed * 2f * Time.deltaTime;
+        currentGoalWheel = new Vector2(
+            MathU.MoveTowardsRange(currentWheel.x, targetWheelPos.x, currentSpeed, 36f),
+            Mathf.MoveTowards(currentWheel.y, targetWheelPos.y, currentSpeed)
+            );
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 target = regionLeader.RegionPosition(currentGoalWheel.x, currentGoalWheel.y);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        wheel ??= GameObject.FindGameObjectWithTag("Wheel").GetComponent<Wheel>();
+        regionLeader ??= wheel.regions[0];
+
+        Vector2 target = regionLeader.RegionPosition(currentGoalWheel.x, currentGoalWheel.y);
+        Gizmos.color = Color.white;
+
+        if (currentWheel != currentGoalWheel)
+            Gizmos.DrawLine(transform.position, target);
+    }
+}
