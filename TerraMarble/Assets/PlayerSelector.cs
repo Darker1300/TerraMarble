1c0f774f55d597eeda9477337a627baa2d7e049e
Tomato <tomas.k.munro@gmail.com>
2022-11-17 11:42:41 +1100

    gg

--- TerraMarble/Assets/PlayerSelector.cs
+++ TerraMarble/Assets/PlayerSelector.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerSelector : MonoBehaviour
 {
+    public List<GameObject> Balls;
+    public GameObject currentObj;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,43 @@ public class PlayerSelector : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //drag out angle 
+
+    //check for closest ball drag angle and select it
+    //this happens when the min is exceeded and 
+    //if player delta exceeds lock
+    // rotate offset to this
+
+    //if goes below min it deselects
+
+    ////////////////////////////////////////////
+    ///get angle , get closest object, s
+    //public float GetAngle()
+    //{
+    //    currentObj = Balls[0];
+    //    for (int i = 0; i < Balls.Count; i++)
+    //    {
+    //        //compare sign angle
+    //        //if less than current then overwrite current
+
+    //    }
+    
+    //}
+
+    public Vector2 RotateToAngle(Vector2 AimDirection, float angle)
+    {
+       // AimDirection = (Vector2)(Quaternion.Euler(0, 0, angle) * updateGravityScript.direction.normalized);
+       
+        AimDirection = (AimDirection * 20);
+        //Debug.DrawLine(transform.position, transform.position + transform.rotation * (Vector3)AimDirection, Color.red);
+        //Debug.DrawLine(transform.position, transform.position + transform.rotation * (Vector3)dirFake, Color.blue);
+        return AimDirection;
+    }
+    public void swapOut()
+    { 
+    
+    
     }
 }
