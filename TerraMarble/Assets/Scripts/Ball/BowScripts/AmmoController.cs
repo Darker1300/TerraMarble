766c2e7943a00558cf5dbe554f9035dfaa97a1a6
Tomato <tomas.k.munro@gmail.com>
2022-11-28 19:31:51 +1100

    bow Functionality complete,  needs tweaks( stop ball colliding with enemies or loosing so much velocity when it hits them)

--- TerraMarble/Assets/Scripts/Ball/BowScripts/AmmoController.cs
+++ TerraMarble/Assets/Scripts/Ball/BowScripts/AmmoController.cs
@@ -6,23 +6,28 @@ public class AmmoController : MonoBehaviour
 {
     [SerializeField]
     private ObjectPooler pooler;
-    [SerializeField]
-    private GameObject currentProjectile;
+  
+    public GameObject currentProjectile;
+    
     //
-
+    
     //object pooler 
     //arrow prefab will have abillity to change types , flame arrow , ect 
 
     ///GET ARROW FROM POOL
     ///pass in type 
-    public void GetProjectile(BallStateTracker.BallState state)
+    public void GetProjectile(BallStateTracker.BallState state,Vector2 pos)
     {
-       currentProjectile =  pooler.SpawnFromPool(transform.position, this.gameObject.transform, false);
+       currentProjectile =  pooler.SpawnFromPool(transform.position, null, true);
         //MAKE SURE PROJECTILE FROM POOL IS CONFIGURED
         currentProjectile.GetComponent<Projectile>().StateConfigure(state);
+        currentProjectile.GetComponent<Projectile>().TargetDirection = pos - (Vector2)transform.position;
+        currentProjectile.GetComponent<Projectile>().pooler = pooler;
+
+
         //PASS IN TARGET 
 
-    
+
     }
     ///CONFIGURE AMMO TYPE
     ///
@@ -34,12 +39,13 @@ public class AmmoController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         pooler = GetComponent<ObjectPooler>();
+        pooler.CreatePool(20);   
     }
 
+
+
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
