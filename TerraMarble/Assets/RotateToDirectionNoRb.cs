b978453dff945a7b59db047ce20e748cf72d0551
Ethan Moffatt <102685218@student.swin.edu.au>
2023-01-24 17:07:56 +1100

    Updated Ethan 8 sky with tree paddles

--- TerraMarble/Assets/RotateToDirectionNoRb.cs
+++ TerraMarble/Assets/RotateToDirectionNoRb.cs
@@ -2,41 +2,43 @@ using System.Collections;
 using UnityEngine;
 
 
-
 public class RotateToDirectionNoRb : MonoBehaviour
 {
-
     public float rotationSpeed = 25;
     public float moveSpeed = 25;
     public Vector2 direction;
+
     private Quaternion starting;
+
+    private Transform targetTransform;
+
+    private TreeBend treeBend;
     //public float PaddleBackSpeed;
 
     //Coroutine stuff
-    [SerializeField]
-    private float duration;
+    [SerializeField] private float duration;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        starting = transform.rotation;
+        targetTransform = transform.parent;
+        starting = targetTransform.localRotation;
         InputManager.LeftDragEvent += RotateBack;
+        treeBend = FindObjectOfType<TreeBend>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-      
-        
-
         // transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
-    public void RotateToThis(Vector2 newDirection,float percentage,Vector3 pos)
+
+    public void RotateToThis(Vector2 newDirection, float percentage, Vector3 pos)
     {
-        var relativePoint = transform.InverseTransformPoint(pos);
+        var relativePoint = targetTransform.InverseTransformPoint(pos);
         if (relativePoint.x <= 0.0)
         {
-            newDirection.y = -1 ;
+            newDirection.y = -1;
             Debug.Log("R");
             //Mathf.Abs (newDirection.x) ;
         }
@@ -44,52 +46,41 @@ public class RotateToDirectionNoRb : MonoBehaviour
         {
             Debug.Log("L");
             newDirection.y = 1;
-
-
         }
 
         float angle = Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg;
         //angle += offset;
-        angle *= (1-percentage) ;
+        angle *= 1 - percentage;
 
         //float dot = Vector2.Dot(-transform.parent.up, Direction);
-       
 
 
         //angle = Vector2.Dot(,)
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward) * starting;
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-
+        targetTransform.localRotation = Quaternion.Slerp(targetTransform.localRotation, rotation, rotationSpeed * Time.deltaTime);
     }
+
     public void RotateBack(bool keyDown)
     {
-        if (!keyDown)
-        { 
-        StartCoroutine(ReturnToDefaultAngle());
-        }
-
+        if (!keyDown) treeBend.StartCoroutine(ReturnToDefaultAngle());
     }
-    IEnumerator ReturnToDefaultAngle()
+
+    private IEnumerator ReturnToDefaultAngle()
     {
-        float Timer= 0;
+        float Timer = 0;
 
 
-
-        while (Timer <= duration )
+        while (Timer <= duration)
         {
-
             Timer = Timer + Time.deltaTime;
             float percent = Mathf.Clamp01(Timer / duration);
 
             float angle = Mathf.Atan2(0, 1) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward) * starting;
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, percent);
-
-
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward) * starting;
+            targetTransform.localRotation = Quaternion.Slerp(targetTransform.localRotation, rotation, percent);
 
 
             yield return null;
         }
-
     }
-    }
+}
--- TerraMarble/Assets/Scripts/treess/TreeBend.cs
+++ TerraMarble/Assets/Scripts/treess/TreeBend.cs
@@ -7,97 +7,82 @@ using Shapes;
 
 public class TreeBend : MonoBehaviour
 {
+    [SerializeField] private string TargetTag;
+    [SerializeField] private Vector2 direction;
 
-    [SerializeField]
-    private string TargetTag;
-    [SerializeField]
-    private Vector2 direction;
-  
-    public List<GameObject> nearbyTargets = new List<GameObject>();
-    [SerializeField]
-    private Vector2 WheelPosition;
-    [SerializeField]
-    private Transform wheel;
+    public List<GameObject> nearbyTargets = new();
+    [SerializeField] private Vector2 WheelPosition;
+    [SerializeField] private Transform wheel;
     private float min = 13;
-    private float max= 19f;
+
+    private float max = 19f;
+
     // Start is called before the first frame update
     private float coliderRadius;
-    [SerializeField]
-    private AnimationCurve treeBend;
+    [SerializeField] private AnimationCurve treeBend;
     private AimTreeLockUI aimUi;
 
     //
-    
-    void Start()
+
+    private void Start()
     {
-        aimUi = GameObject.FindObjectOfType<AimTreeLockUI>();
+        aimUi = FindObjectOfType<AimTreeLockUI>(true);
         //wheel = FindObjectOfType<Wheel>();
         coliderRadius = GetComponent<CircleCollider2D>().radius;
 
         InputManager.LeftDragVectorEvent += UpdatePosition;
-
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         UpdateTrees();
 
-       // GameObject[] ffgfg = Physics2D.OverlapCircle(WheelPosition, 5,,);
+        // GameObject[] ffgfg = Physics2D.OverlapCircle(WheelPosition, 5,,);
     }
 
     private void UpdateTrees()
     {
-
-
         if (nearbyTargets.Count > 0)
-        {
             foreach (var target in nearbyTargets)
             {
                 //coliderRadius
-             
 
-                float percent = treeBend.Evaluate( Mathf.Clamp01( (target.transform.position - transform.position).magnitude / coliderRadius));
-                target.GetComponent<RotateToDirectionNoRb>().RotateToThis(  direction,percent, ( transform.position ));
-                
 
+                float percent =
+                    treeBend.Evaluate(Mathf.Clamp01((target.transform.position - transform.position).magnitude /
+                                                    coliderRadius));
+                target.GetComponent<RotateToDirectionNoRb>().RotateToThis(direction, percent, transform.position);
             }
-        }
-        
     }
-    public void UpdatePosition(Vector2 dir,Vector2 delta)
+
+    public void UpdatePosition(Vector2 dir, Vector2 delta)
     {
         //get the difference and reverse it
-        float diference = (max - Mathf.Clamp(dir.magnitude, min, max));
+        float diference = max - Mathf.Clamp(dir.magnitude, min, max);
         diference = min + diference;
 
         //invert
-        transform.position = -((Vector3)dir.normalized *  diference);
+        transform.position = -((Vector3)dir.normalized * diference);
         //Mathf.Clamp(dir.magnitude, min, max)
-       // aimUi.BarIncrease( Mathf.Clamp01((target.transform.position - transform.position).magnitude / coliderRadius));
+        // aimUi.BarIncrease( Mathf.Clamp01((target.transform.position - transform.position).magnitude / coliderRadius));
     }
-    
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("dddsjd");
-        if (collision.gameObject.CompareTag(TargetTag))
-        {
-            
-            nearbyTargets.Add(collision.gameObject);
-
-        }
-
+        var rotToDir = collision.gameObject.GetComponentInParent<RotateToDirectionNoRb>();
+        if (rotToDir) nearbyTargets.Add(rotToDir.gameObject);
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-
         if (collision.gameObject.CompareTag(TargetTag))
         {
-            
-            nearbyTargets.Remove(collision.gameObject);
-            collision.gameObject.GetComponent<RotateToDirectionNoRb>()?.StartCoroutine("ReturnToDefaultAngle");
+            var rotToDir = collision.gameObject.GetComponentInParent<RotateToDirectionNoRb>();
+            nearbyTargets.Remove(rotToDir.gameObject);
+            rotToDir?.StartCoroutine("ReturnToDefaultAngle");
         }
     }
-
 }
