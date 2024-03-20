96b6aee7014ff7eef418a85a696c155cef0662a1
Ethan Moffatt <102685218@student.swin.edu.au>
2024-03-20 16:26:10 +1100

    updated scene

--- TerraMarble/Assets/WetComp.cs
+++ TerraMarble/Assets/WetComp.cs
@@ -6,79 +6,79 @@ using Shapes;
 
 public class WetComp : MonoBehaviour
 {
-	bool isRegion = false;
-	[SerializeField] private Region parentRegion;
-	[SerializeField] private ShapeRenderer shape;
+    private bool isRegion = false;
+    [SerializeField] private Region parentRegion;
+    [SerializeField] private ShapeRenderer shape;
     [SerializeField] private SpriteRenderer sprite;
 
     private bool usingShape;
-	//private Color StartColor;
+
+    //private Color StartColor;
     private void Start()
     {
-        if (GetComponent<ShapeRenderer>() !=null)
+        if (GetComponent<ShapeRenderer>() != null)
         {
-		    
-            shape =  GetComponent<ShapeRenderer>();
+            shape = GetComponent<ShapeRenderer>();
             usingShape = true;
-
         }
         else
         {
             sprite = GetComponent<SpriteRenderer>();
         }
-		//MakeTransparent();
-	}
 
-    void OnEnable()
-	{
+        //MakeTransparent();
+    }
+
+    private void OnEnable()
+    {
         parentRegion = GetComponentInParent<Region>();
 
         MakeTransparent();
         //parentRegion.comp.AddListener(UpdateWet);
         parentRegion.GetComponent<RegionWetController>().WetUpdate += UpdateWet;
-
-
     }
-	public void SubscribeToRegionWetComp()
-	{
-		parentRegion.GetComponent<RegionWetController>().WetUpdate += UpdateWet;
-	}
-	public void UnsubscribeToRegionWetComp()
-	{
-		parentRegion.GetComponent<RegionWetController>().WetUpdate -= UpdateWet;
-	}
-	
-	public void MakeTransparent()
-	{
-        //shape.Color = Color.blue;
-	}
-	void OnDisable()
-	{
-		parentRegion.GetComponent<RegionWetController>().WetUpdate += UpdateWet;
-	}
 
-	public void UpdateWet(float percent)
-	{
-		// apply color change to renderer
+    public void SubscribeToRegionWetComp()
+    {
+        parentRegion.GetComponent<RegionWetController>().WetUpdate += UpdateWet;
+    }
+
+    public void UnsubscribeToRegionWetComp()
+    {
+        parentRegion.GetComponent<RegionWetController>().WetUpdate -= UpdateWet;
+    }
+
+    public void MakeTransparent()
+    {
+        //shape.Color = Color.blue;
+    }
+
+    private void OnDisable()
+    {
+        parentRegion.GetComponent<RegionWetController>().WetUpdate += UpdateWet;
+    }
+
+    public void UpdateWet(float percent)
+    {
+        // apply color change to renderer
         if (usingShape)
         {
-            shape.Color = SetAlpha(shape.Color, percent / 100);
-
+            if (shape)
+                shape.Color = SetAlpha(shape.Color, percent / 100);
         }
-        else
+        else if (sprite)
+        {
             sprite.color = SetAlpha(sprite.color, percent / 100);
+        }
+
         //Debug.Log("wetAdded" + percent / 100);
         //shape.Color = Color.blue;
-
     }
 
-	
-	
 
-	private Color SetAlpha(Color _color, float _a)
-	{
-		_color.a = _a;
-		return _color;
-	}
-	
+    private Color SetAlpha(Color _color, float _a)
+    {
+        _color.a = _a;
+        return _color;
+    }
 }
